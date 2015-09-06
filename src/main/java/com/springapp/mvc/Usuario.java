14ef41f44970da1a6ccfbff50bb470c83599056f
coco <jgalloni91@gmail.com>
2015-09-05 23:36:07 -0300

    asdsad

--- src/main/java/com/springapp/mvc/Usuario.java
+++ src/main/java/com/springapp/mvc/Usuario.java
@@ -2,11 +2,20 @@ package com.springapp.mvc;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+
 /**
  * Created by coco on 02/09/2015.
  */
-public class Usuario implements Serializable {
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.MINIMAL_CLASS, include = As.PROPERTY, property = "@class")
+public class Usuario{
+    @JsonProperty("nobmre")
     private String nombre;
+    @JsonProperty("pass")
     private String pass;
 
     public String getNombre() {
@@ -23,5 +32,9 @@ public class Usuario implements Serializable {
 
     public void setPass(String pass) {
         this.pass = pass;
+    }
+
+    @Override public String toString(){
+        return "Usuario [nombre=" + nombre + ", pass=" + pass + "]";
     }
 }
